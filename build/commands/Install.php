adb2f61b462dcc22d3923bc134937a636a9547da
Wycliffe Omondi Otieno <wycliffomondiotieno@gmail.com>
2026-07-05 09:19:10 +0300

    Saqle installation command update

--- build/commands/Install.php
+++ build/commands/Install.php
@@ -8,14 +8,14 @@ class Install {
 
          Cli::print("Installing SaQle...\n");
          
-         $this->copy_env();
+         self::copy_env();
 
          Cli::print("Installation complete!\n");
 
          return 0;
      }
 
-     private function copy_env(): void {
+     private static function copy_env(): void {
 
          Cli::print("Creating a development .env file\n");
 
@@ -23,12 +23,12 @@ class Install {
          $destination_dir = path_join([config('base_path'), 'env', 'development']);
          $destination_file = path_join([$destination_dir, '.env']);
 
-         if(file_exists($destination)){
+         if(file_exists($destination_file)){
              Cli::print(".env file already exists");
              return;
          }
 
-         if(!file_exists($origin)){
+         if(!file_exists($origin_file)){
              Cli::print(".env.example file not found!");
              return;
          }
