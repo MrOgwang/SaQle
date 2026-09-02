5f0a8c1bc4b39dfd9ae8a232e81b85296e15e6ff
Wycliffe Omondi Otieno <wycliffomondiotieno@gmail.com>
2026-09-02 05:59:41 +0300

    Creating logs dir when installing saqle

--- src_tmp/Build/Commands/Install.php
+++ src_tmp/Build/Commands/Install.php
@@ -17,6 +17,12 @@ class Install extends Command {
      public function handle(CommandContext $context) : int {
          Cli::print("Installing SaQle...\n");
 
+         //create a logs dir
+         $logs_dir = path_join([config('base_path'), 'logs']);
+         if(!is_dir($logs_dir)){
+             saqle_dir()->create($logs_dir);
+         }
+
          //create a .env file
          new MakeEnv()->handle($context);
 
@@ -26,7 +32,7 @@ class Install extends Command {
          Cli::print("Saqle installation complete! Next steps to finish your setup >\n");
          Cli::print("1. Update email and database details in development/.env");
          Cli::print("2. Cd into the project folder and run the following commands:");
-         Cli::print(">> php saqle make:migrations -n=Initial");
+         Cli::print(">> php saqle make:migrations Initial");
          Cli::print(">> php saqle migrate");
          Cli::print(">> php saqle make:superuser");
          Cli::print("\n");
